9f7e5b7576d6bf3ef3cc566937bedd7788e69599
Harsh <apple@Harshs-MacBook-Air-4.local>
2026-02-17 13:43:30 +0530

    Ensure consistent 13/1-A address in Contact page and Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -26,7 +26,7 @@ const Footer = () => {
                 <div className={styles.column}>
                     <h4>Contact Us</h4>
                     <p><strong>Registered Office:</strong> Siyaram Nagar, Ranchi Road, Marar, Dist. Ramgarh - 82917 (Jharkhand)</p>
-                    <p><strong>Works:</strong> Plot No. 48, Village- Konra, Barhi Industrial Area, Dist.- Hazaribagh - 825405 (Jharkhand)</p>
+                    <p><strong>Works:</strong> 13/1-A, Village Konra, Barhi Industrial Area, Barhi, Hazaribagh - 825405 (Jharkhand)</p>
                     <p><strong>Phone:</strong> +91 94319 41464, +91 82109 49940</p>
                     <p><strong>Email:</strong> jsvent2018@gmail.com</p>
                 </div>
