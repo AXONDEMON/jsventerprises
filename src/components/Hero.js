a32cff3580fc0cc5311cad6377e450251ef256e3
Harsh <apple@Harshs-MacBook-Air-4.local>
2026-03-15 16:39:32 +0530

    Update Hero image to factory main and restore mobile CTA buttons

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -10,7 +10,7 @@ const Hero = () => {
             <div className={styles.sliderBg}>
                 <div
                     className={styles.bgImage}
-                    style={{ backgroundImage: "url('/images/image copy 3.png')" }}
+                    style={{ backgroundImage: "url('/images/hero_factory_main.png')" }}
                 />
             </div>
 
